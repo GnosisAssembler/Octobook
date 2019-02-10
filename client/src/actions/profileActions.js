fa61e8c78a51d02b5b5d35d88386b054e2896435
Panos Karypiadis <pan.karyp@gmail.com>
2019-02-10 16:12:33 +0200

    Add Experience functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -34,12 +34,25 @@ export const createProfile = (profileData, history) => dispatch => {
     .post('/api/profile', profileData)
     .then(res => history.push('/dashboard'))
     .catch(err =>
-    dispatch({
-        type: GET_ERRORS,
-        payload: err.response.data
-    })
+        dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data
+        })
     );
 };
+
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+    axios
+        .post('/api/profile/experience', expData)
+        .then(res => history.push('/dashboard'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+}
 
 // Delete account and profile
 export const deleteAccount = () => dispatch => {
