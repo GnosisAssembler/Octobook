9b8b7509ae634c69a3f6a9742a6f51c365736739
Panos Karypiadis <pan.karyp@gmail.com>
2019-02-12 15:07:31 +0200

    Add enviromental variables and heroku based build

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //mongoURI: 'mongodb://localhost/mernsocialapp',
+    mongoURI: 'mongodb://pankaryp3:pankaryp3@ds131765.mlab.com:31765/mini-social-app',
+    secretOrKey: 'secret'
+};
